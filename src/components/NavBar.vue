55584f95f3d5c80fee21b3529a2a0344cfe32789
Rayan <r.a.alsamari@gmail.com>
2023-09-14 22:04:09 +0300

    Added navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <ul >
+      <li><router-link to="/">الرئيسية</router-link></li>
+      <li><router-link to="/schedules">الجداول</router-link></li>
+      <li><router-link to="/profile">الملف الشخصي</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  background-color: #333;
+  height: 80px;
+  width: 100%;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: flex-start;
+}
+
+li {
+  margin: 10px;
+  padding: 20px;
+  text-decoration: none ;
+}
+
+a {
+  text-decoration: none;
+  color: white; 
+}
+
+.router-link-active {
+  font-weight: bold;
+  background: crimson;
+  border-radius: 10px;
+  padding: 15px;
+}
+</style>
+
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import Schedules from '../views/Schedules.vue'
+import Profile from '../views/Profile.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,14 +10,16 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+   {
+    path: '/schedules',
+    name: 'schedules',
+    component: Schedules
+   },
+   {
+    path: '/Profile',
+    name: 'profile',
+    component: Profile
+   }
   ]
 })
 
