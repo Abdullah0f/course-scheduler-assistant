32fe760d9065913c3407527343581920a5167a98
Abdullah Fahad <abdullah84f@gmail.com>
2023-09-20 22:39:07 +0300

    disable base

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/main.css'
-// import 'normalize.css'
+import 'normalize.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
