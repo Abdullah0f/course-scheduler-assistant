516c6cb6ac347a9ce7e8f5627b2a8b278072e904
Abdullah Fahad <abdullah84f@gmail.com>
2023-09-29 23:10:50 +0300

    show num of possible schdules

--- src/views/TheSchedules.vue
+++ src/views/TheSchedules.vue
@@ -3,6 +3,7 @@
   <ScheduleComponent :schedule="schedule" />
   <ChooseCourses :courses="transformedCourses" v-if="Object.keys(courses).length !== 0" @submit-courses="handleCourses"/>
   <div v-if="schedules" class="flex flex-column items-center">
+      <h2>عدد الجداول الممكنة <span class="text-blue-600 mr-2">{{ schedules.length }}</span></h2>
       <div v-for="schedule in schedules" :key="schedule.id">
         <ScheduleComponent :schedule="schedule" />
       </div>
