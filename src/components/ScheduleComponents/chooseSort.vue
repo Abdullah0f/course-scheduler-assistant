90ae06834ae56be03faf21512b7823b893ff26bf
Rayan <r.a.alsamari@gmail.com>
2023-10-29 20:02:24 +0300

    seperated sort order from the dropdown menu

--- src/components/ScheduleComponents/chooseSort.vue
+++ src/components/ScheduleComponents/chooseSort.vue
@@ -1,34 +1,60 @@
 <template>
     <div class="mt-3 flex align-items-center gap-3">
         <label for="sort">رتب حسب</label>
-        <Dropdown v-model="choosenSort" :options="sorts" optionValue="value" optionLabel="name" placeholder="رتب حسب" class="w-15rem" :pt="{input: 'font'}"/>
+        <Dropdown v-model="chosenSort" :options="sorts" optionValue="value" optionLabel="name" placeholder="رتب حسب" class="w-15rem" :pt="{input: 'font'}"/>
+        <div v-if="chosenSort">
+            <Button
+            @click="setOrder('asc')" 
+            icon="pi pi-sort-amount-up" 
+            iconPos="right" 
+            class="p-button-text" />
+
+            <Button
+            @click="setOrder('desc')" 
+            icon="pi pi-sort-amount-down" 
+            iconPos="right" 
+            class="p-button-text"/>
+        </div>
+        
+
     </div>
 </template>
 
 <script setup>
 import Dropdown from 'primevue/dropdown';
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
+import Button  from 'primevue/button';
 
 const props = defineProps({
-    sort: {
+    sortCriterion: {
         type: String,
         required: true
+    },
+    sortOrder: {
+        type: String,
+        default: 'asc'
     }
 })
 const emit = defineEmits(['sort-changed'])
 
-const choosenSort = ref(props.sort);
+const chosenSort = ref(props.sortCriterion);
+const order = ref(props.sortOrder);
+
 const sorts = ref([
-    {name:"طول الجدول تصاعدي", value: "timeDiff-asc"},
-    {name:"طول الجدول تنازلي", value: "timeDiff-desc"},
-    {name:"عدد ايام الاوف", value: "daysOff-desc"},
-    {name:"عدد البريكات تصاعدي", value: "breaks-asc"},
-    {name:"عدد البريكات تنازلي", value: "breaks-desc"},    
+    {name:"طول الجدول", value: "timeDiff"},
+    {name:"عدد ايام الاوف", value: "daysOff"},
+    {name:"عدد البريكات", value: "breaks"},
 ])
 
-watch(choosenSort, (newVal) => {
-  emit('sort-changed', newVal);
-});
+function setOrder(newOrder) {
+    order.value = newOrder;
+    emitSortChange();
+}
+
+function emitSortChange() {
+    emit('sort-changed', `${chosenSort.value}-${order.value}`);
+}
+
 </script>
 
 <style lang="scss" scoped>
