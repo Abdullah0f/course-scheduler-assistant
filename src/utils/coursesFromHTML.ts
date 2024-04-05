db1caa901d960568f2af4125964062ef0a42e9ee
Abdullah Fahad <abdullah84f@gmail.com>
2024-04-05 17:18:57 +0300

    convert the project to TypeScript

--- src/utils/coursesFromHTML.ts
+++ src/utils/coursesFromHTML.ts
@@ -1,22 +1,44 @@
 const ROWS_SELECTOR = 'tbody .ROW1, tbody .ROW2'
 
-export function getCourses(htmlString) {
+interface Period {
+  days: RegExpMatchArray | null
+  time: string
+  startTime: number
+  endTime: number
+  location: string
+  classType: string
+  instructor: string
+}
+
+interface Course {
+  code: string;
+  name: string;
+  classCode: string | undefined;
+  type: string | undefined;
+  hours: string | undefined;
+  open: string | undefined;
+  instructor: string | undefined;
+  periods: Period[];
+  exam: string | undefined;
+}
+
+export function getCourses(htmlString: string): Record<string, Course[]> {
   const parser = new DOMParser()
   const doc = parser.parseFromString(htmlString, 'text/html')
 
   const rows = doc.querySelectorAll(ROWS_SELECTOR)
   const isOfferedCourses = rows[Math.floor(rows.length / 2)].querySelectorAll('td').length === 7
-  const courses = {}
+  const courses : Record<string, Course[]>= {}
   rows.forEach((row) => {
     const cells = row.querySelectorAll('td')
     let i = 0 //because if not attempted courses, then we skip 5th cell
-    const code = cells[i++].textContent
-    const name = cells[i++].textContent
-    const classCode = cells[i++]?.textContent
-    const type = cells[i++]?.textContent
-    const hours = cells[i++]?.textContent
+    const code = cells[i++].textContent || ''
+    const name = cells[i++].textContent || ''
+    const classCode = cells[i++]?.textContent || undefined
+    const type = cells[i++]?.textContent || undefined
+    const hours = cells[i++]?.textContent || undefined
     if (!isOfferedCourses) i++ //to skip gender column
-    const open = cells[i++]?.textContent
+    const open = cells[i++]?.textContent || undefined
     const instructor = cells[i]?.querySelectorAll('input')[0]?.value
     const periodsString = cells[i]?.querySelectorAll('input')[1]?.value
     const exam = cells[i]?.querySelectorAll('input')[2]?.value
@@ -30,7 +52,7 @@ export function getCourses(htmlString) {
       previousCourse.periods = [...previousCourse.periods, ...periods]
       return
     }
-    const course = {
+    const course : Course= {
       code,
       name,
       classCode,
@@ -49,67 +71,70 @@ export function getCourses(htmlString) {
   })
   return courses
 }
-export function generatePeriods(periodsString, type, instructorName) {
+export function generatePeriods(periodsString: string, type: string | undefined, instructorName: string | undefined): Period[] {
   if (!periodsString) return []
-  const periods = []
-  const numOfPeriods = [...periodsString.matchAll(/@t/g)].length //since @t is the delimiter between diffrent periods
+  const periods: Period[] = []
+  const numOfPeriods = Array.from(periodsString.matchAll(/@t/g)).length; //since @t is the delimiter between diffrent periods
   for (let i = 0; i < numOfPeriods; i++) {
     const periodString = periodsString.split('@n')[i]
     const days = periodString.split('@t')[0].trim().match(/\d/g)
-    const time = periodString.match(/@t\s*(.*?)\s*@r/)[1]?.trim()
-    const { startTime, endTime } = extractTime(time)
-    const location = periodString.match(/@r\s*(.*)/)[1]?.trim()
+    const time = periodString.match(/@t\s*(.*?)\s*@r/)?.[1]?.trim()
+    const { startTime, endTime } = time? extractTime(time) : { startTime: 0, endTime: 0 }
+    const location = periodString.match(/@r\s*(.*)/)?.[1]?.trim()
     const classType = type
     const instructor = instructorName
-    const period = {
+    const period : Period = {
       days,
-      time,
+      time : time || '',
       startTime,
       endTime,
-      location,
-      classType,
-      instructor
+      location: location || '',
+      classType: classType || '',
+      instructor: instructor || ''
     }
     periods.push(period)
   }
   return periods
 }
 
-function thereIsAnotherPeriodWithSameType(course, type) {
+function thereIsAnotherPeriodWithSameType(course : Course, type: string|undefined) {
   return course.periods.some((period) => period.classType === type)
 }
-function createAnotherCourseWithThisPeriod(course, periods, type) {
-  const newCourse = { ...course, periods: [] }
+function createAnotherCourseWithThisPeriod(course: Course, periods: Period[], type: string|undefined) {
+  const newCourse: Course = { ...course, periods: [] }
   // remove same type periods and add them to the new course
   newCourse.periods = course.periods.filter((period) => period.classType !== type)
   newCourse.periods = [...newCourse.periods, ...periods]
   return newCourse
 }
 
-export function extractTime(time) {
+export function extractTime(time: string) : { startTime: number, endTime: number } {
   time = time.trim()
 
   // Split the time string using " - " as the delimiter
   const timeParts = time.split(' - ')
 
   // Extract the start time and end time
-  let startTime = convertTo24HourFormat(timeParts[0])
-  let endTime = convertTo24HourFormat(timeParts[1])
+  let startTime : string = convertTo24HourFormat(timeParts[0])
+  let endTime : string = convertTo24HourFormat(timeParts[1])
 
   // Convert the times to Date objects
-  startTime = timeToTimestamp(startTime)
-  endTime = timeToTimestamp(endTime)
+  const startTimeTimestamp = timeToTimestamp(startTime)
+  const endTimeTimestamp = timeToTimestamp(endTime)
 
-  return { startTime, endTime }
+  return { 
+    startTime: startTimeTimestamp,
+    endTime: endTimeTimestamp
+  }
 }
-function convertTo24HourFormat(time) {
+function convertTo24HourFormat(time : string) : string {
   // get ص or م
   const amOrPm = time.slice(-1)
   // remove ص or م
   time = time.slice(0, -1)
   // split time string into hours and minutes
   const timeParts = time.split(':')
-  let hours = timeParts[0]
+  let hours: any = timeParts[0]
   let minutes = timeParts[1]
   // convert to 24-hour format
   if (amOrPm === 'ص') {
@@ -120,7 +145,7 @@ function convertTo24HourFormat(time) {
   return (hours + ':' + minutes).trim()
 }
 
-function timeToTimestamp(time) {
+function timeToTimestamp(time: string) : number {
   const date = new Date('1970-01-01 ' + time)
   return date.getTime()
 }
